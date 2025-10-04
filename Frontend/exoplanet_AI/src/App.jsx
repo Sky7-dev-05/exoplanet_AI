07d7d150cafe800766b6af65d5c0f37a092f705f
Friedbocovo <friedbocovo03@gmail.com>
2025-10-04 12:45:16 +0200

    first commit

---
+++ Frontend/exoplanet_AI/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 className='text-5xl'>Hello Team</h1>
+    </div>
+  )
+}
+
+export default App
