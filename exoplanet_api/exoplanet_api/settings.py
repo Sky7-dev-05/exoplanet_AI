c94f9499bf064399381af607b84e380a213eb321
Ing-Nahine <combarinahine@gmail.com>
2025-10-05 19:23:53 +0100

    Update settings.py

--- exoplanet_api/exoplanet_api/settings.py
+++ exoplanet_api/exoplanet_api/settings.py
@@ -64,13 +64,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'exoplanet_api.wsgi.application'
 
+# Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
